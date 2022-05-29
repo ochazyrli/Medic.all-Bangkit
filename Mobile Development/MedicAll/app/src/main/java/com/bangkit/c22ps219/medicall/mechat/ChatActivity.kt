5d53569313d967cb80d25dd02ca2774bb3f40e13
ariasrupawansyah <a2291f2474@bangkit.academy>
2022-05-29 15:52:04 +0700

    Membuat project starter awal untuk capstone project medic all

---
+++ Mobile Development/MedicAll/app/src/main/java/com/bangkit/c22ps219/medicall/mechat/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.c22ps219.medicall.mechat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.c22ps219.medicall.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
