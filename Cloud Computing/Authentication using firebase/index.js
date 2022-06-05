8c1910f4ca6866f433a9646ca798d676f2bf8b87
Ramandhika <ramandhika44@gmail.com>
2022-06-05 19:13:44 +0700

    Update Signout

--- Cloud Computing/Authentication using firebase/index.js
+++ Cloud Computing/Authentication using firebase/index.js
@@ -1,50 +1,47 @@
 const express = require("express");
-const bodyParser = require("body-parser")
-const path = require('path')
+const bodyParser = require("body-parser");
+const path = require("path");
 
-
-
-require('firebase/auth')
+require("firebase/auth");
 
 app = express();
-app.use('/', express.static(path.join(__dirname, 'static')))
-app.use(bodyParser.json())
+app.use("/", express.static(path.join(__dirname, "static")));
+app.use(bodyParser.json());
 
 const userService = require("./user_service.js");
 
 app.post("/api/register", async (req, res) => {
   const { Email, passwords } = req.body;
 
-	if (passwords.length < 5) {
-		return res.json({
-			status: 'error',
-			error: 'Password too small. Should be atleast 6 characters'
-		})
-	}
+  if (passwords.length < 5) {
+    return res.json({
+      status: "error",
+      error: "Password too small. Should be atleast 6 characters",
+    });
+  }
 
   try {
     const user = await userService.addUser(Email, passwords);
     await userService.verifyemail();
-    res.json({ status: 'ok' })
+    res.json({ status: "ok" });
   } catch (err) {
     res.status(401).json({ error: err.message });
   }
 });
 
+app.post("/api/login", async (req, res) => {
+  const { Email, passwords } = req.body;
 
-app.post('/api/login', async (req, res) => {
-	const { Email, passwords } = req.body
-
-	if (!Email) {
-		return res.json({ status: 'error', error: 'Invalid Email/password' })
-	}
+  if (!Email) {
+    return res.json({ status: "error", error: "Invalid Email/password" });
+  }
 
   try {
     const user = await userService.authenticate(Email, passwords);
     if (!userService.emailVerified()) {
-    res.status(401).json({ error: 'Please Verify Your Email Address!' });
-    }else{
-    res.json({ status: 'ok' })
+      res.status(401).json({ error: "Please Verify Your Email Address!" });
+    } else {
+      res.json({ status: "ok" });
     }
     //res.json({ status: 'ok' })
   } catch (err) {
@@ -52,16 +49,23 @@ app.post('/api/login', async (req, res) => {
   }
 });
 
-
-app.post('/api/changepas', async (req, res) => {
-  const { Email } = req.body
-  try{
+app.post("/api/changepas", async (req, res) => {
+  const { Email } = req.body;
+  try {
     const user = await userService.changepass(Email);
-    res.json({ status: 'ok' })
+    res.json({ status: "ok" });
   } catch (err) {
     res.status(401).json({ error: err.message });
   }
+});
 
+app.post("/api/signout", async (req, res) => {
+  try {
+    const user = await userService.signOut();
+    res.json({ status: "ok" });
+  } catch (err) {
+    res.status(401).json({ error: err.message });
+  }
 });
 
 const port = process.env.PORT || 3000;
--- Cloud Computing/Authentication using firebase/user_service.js
+++ Cloud Computing/Authentication using firebase/user_service.js
@@ -4,32 +4,26 @@ const firebase = require("firebase/app");
 require("firebase/auth");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLalJ7ZbXZS7s_74S70KLKP3AP9kw6HPg",
-  authDomain: "my-firebase-chat-14c91.firebaseapp.com",
-  databaseURL: "https://my-firebase-chat-14c91-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "my-firebase-chat-14c91",
-  storageBucket: "my-firebase-chat-14c91.appspot.com",
-  messagingSenderId: "678557426644",
-  appId: "1:678557426644:web:9fab13f08139b3dd67ec41",
-  measurementId: "G-8Y9W3STYTT"
+  apiKey: "AIzaSyAjYlai-TyYlWMNAEHbpwmnNPHRYHFtlqA",
+  authDomain: "medicall-firebase.firebaseapp.com",
+  databaseURL: "https://medicall-firebase-default-rtdb.firebaseio.com",
+  projectId: "medicall-firebase",
+  storageBucket: "medicall-firebase.appspot.com",
+  messagingSenderId: "1006325468737",
+  appId: "1:1006325468737:web:759f2b1bd1049db9f140c0",
+  measurementId: "G-ZFF4YXLKNX",
 };
 
 const fb = firebase.initializeApp(firebaseConfig);
 
-exports.addUser = (email, password) =>
-  fb.auth().createUserWithEmailAndPassword(email, password);
+exports.addUser = (email, password) => fb.auth().createUserWithEmailAndPassword(email, password);
 
-exports.authenticate = (email, password) =>
-  fb.auth().signInWithEmailAndPassword(email, password);
+exports.authenticate = (email, password) => fb.auth().signInWithEmailAndPassword(email, password);
 
-exports.emailVerified = () =>
-  fb.auth().currentUser.emailVerified.valueOf();
+exports.emailVerified = () => fb.auth().currentUser.emailVerified.valueOf();
 
-exports.changepass = (email) =>
-  fb.auth().sendPasswordResetEmail(email);
+exports.changepass = (email) => fb.auth().sendPasswordResetEmail(email);
 
-exports.verifyemail = () =>
-  fb.auth().currentUser.sendEmailVerification();
+exports.verifyemail = () => fb.auth().currentUser.sendEmailVerification();
 
-
-
+exports.signOut = () => fb.auth().currentUser.signOut();
