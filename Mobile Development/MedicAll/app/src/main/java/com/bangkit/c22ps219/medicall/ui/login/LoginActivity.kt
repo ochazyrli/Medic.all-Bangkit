6020c8f745b902113e2c04c8ec7bd5e6fdd102de
ariasrupawansyah <a2291f2474@bangkit.academy>
2022-06-06 09:39:29 +0700

    Memperbaiki tampilan login pada aplikasi

--- Mobile Development/MedicAll/app/src/main/java/com/bangkit/c22ps219/medicall/ui/login/LoginActivity.kt
+++ Mobile Development/MedicAll/app/src/main/java/com/bangkit/c22ps219/medicall/ui/login/LoginActivity.kt
@@ -2,11 +2,21 @@ package com.bangkit.c22ps219.medicall.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.bangkit.c22ps219.medicall.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        supportActionBar?.title = " "
+        supportActionBar?.hide()
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
     }
 }
